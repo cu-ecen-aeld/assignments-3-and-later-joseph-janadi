764b75a25eff13eb850f0324f700cc97c8f49357
jjanadi <jjanadi@Ubuntu.myguest.virtualbox.org>
2026-02-05 11:22:40 -0800

    Assigment 5 Part 1

---
+++ server/aesdsocket.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handler(int sig);
+
+int server_fd = -1;
+int client_fd = -1;
+const char *data_file = "/var/tmp/aesdsocketdata";
+int ret;
+
+int main(int argc, char *argv[])
+{
+    /* Set signal handler for SIGINT and SIGTERM */
+    struct sigaction act;
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    ret = sigaction(SIGINT, &act, NULL);
+    if (ret == -1) { perror("Error"); return -1; }
+    ret = sigaction(SIGTERM, &act, NULL);
+    if (ret == -1) { perror("Error"); return -1; }
+
+    /* Create socket */
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) { perror("Error"); return -1; }
+
+    /* Bind socket to port */
+    char *port_str = "9000";
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo *res;
+    ret = getaddrinfo(NULL, port_str, &hints, &res);
+    if (ret != 0) {
+        printf("getaddrinfo: %s\n", gai_strerror(ret));
+        return -1;
+    }
+    ret = bind(server_fd, res->ai_addr, res->ai_addrlen);
+    if (ret == -1) { perror("Error"); return -1; }
+
+    /* Open or create data file */
+    int datafd;
+    datafd = open(data_file, O_WRONLY | O_APPEND | O_CREAT);
+    if (datafd == -1) { perror("Error"); return -1; }
+
+    /* Listen for connections */
+    int backlog = 1;
+    ret = listen(server_fd, backlog);
+    if (ret == -1) { perror("Error"); return -1; }
+
+    struct sockaddr_in accepted_sockaddr;
+    socklen_t addrlen = sizeof(accepted_sockaddr);
+    ssize_t nread;
+    const size_t BUF_SIZE = 500;
+    char buf[BUF_SIZE];
+    ssize_t nwritten;
+    size_t count;
+    ssize_t nsent;
+    /* Loop until SIGINT or SIGTERM */
+    while (1) {
+        /* Accept next connection */
+        client_fd = accept(server_fd, (struct sockaddr *)&accepted_sockaddr, &addrlen);
+        if (client_fd == -1) { perror("Error"); return -1; }
+        syslog(LOG_DEBUG, "Accepted connection from %u", accepted_sockaddr.sin_addr.s_addr);
+
+        /* Receive data over socket and append to data file */
+        do {
+            nread = recv(client_fd, buf, BUF_SIZE, 0);
+            if (nread == -1) { perror("Error"); return -1; }
+            count = BUF_SIZE < (size_t)nread ? BUF_SIZE : (size_t)nread;
+            nwritten = write(datafd, buf, count);
+            if (nwritten == -1) { perror("Error"); return -1; }
+        } while (nread != 0);
+
+        /* Return contents of data file to client */
+        struct stat statbuf;
+        ret = stat(data_file, &statbuf);
+        if (ret == -1) { perror("Error"); return -1; }
+        nsent = sendfile(client_fd, datafd, 0, statbuf.st_size);
+        if (nsent == -1) { perror("Error"); return -1; }
+
+        /* Close connection */
+        ret = close(client_fd);
+        if (ret == -1 ) { perror("Error"); return -1; }
+        syslog(LOG_DEBUG, "Closed connection from %u", accepted_sockaddr.sin_addr.s_addr);
+    }
+
+    freeaddrinfo(res);
+}
+
+void handler(int sig)
+{
+    /* Complete any open connections */
+    // TODO
+
+    /* Close any open sockets */
+    ret = close(client_fd);
+    if (ret == -1) { perror("Error"); exit(EXIT_FAILURE); }
+
+    /* Delete data file */
+    ret = remove(data_file);
+    if (ret == -1) { perror("Error"); exit(EXIT_FAILURE); }
+    syslog(LOG_DEBUG, "Caught signal, exiting");
+
+    exit(EXIT_SUCCESS);
+}
